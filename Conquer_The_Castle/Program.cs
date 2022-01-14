bf11c929f4b74a9629630fc2e79800b817d544e7
Giorgi Matsaberidze <60402856+giomaca@users.noreply.github.com>
2022-01-14 13:08:30 +0400

    Update Program.cs

--- Conquer_The_Castle/Program.cs
+++ Conquer_The_Castle/Program.cs
@@ -6,12 +6,24 @@ namespace Conquer_The_Castle
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter way length: ");
-            int length = int.Parse(Console.ReadLine());
-            Console.Write("Enter enemy amount: ");
-            int enemy = int.Parse(Console.ReadLine());
-            Game obj = new Game(length, enemy);
+            int length, enemy;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter way length: ");
+                    length = int.Parse(Console.ReadLine());
+                    Console.Write("Enter enemy amount: ");
+                    enemy = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Enter valid input!");
+                }
+            }
 
+            Game obj = new Game(length, enemy);
             obj.SetPosition();
             obj.Play();
         }
